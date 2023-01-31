0a918f3795d897a2946ef7f6bda97bb149cdea72
yolojewjitsu <95731615+yolojewjitsu@users.noreply.github.com>
2023-02-01 02:18:29 +0300

    upd serveronrender

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch("https://codex-seven-psi.vercel.app/", {
+  const response = await fetch("https://codex-6yzp.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
